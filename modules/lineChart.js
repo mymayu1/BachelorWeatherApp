d413a8565d4331c8ab1ee3441c860cb259d25a17
mymayu1 <65132134+mymayu1@users.noreply.github.com>
2025-01-08 17:08:22 +0100

    style improv

--- modules/lineChart.js
+++ modules/lineChart.js
@@ -4,7 +4,7 @@ export function createLineChart(containerId, data, temperatureHiLo) {
         return;
     }
     // Masse und Margins
-    const margin = { top: 10, right: 50, bottom: 50, left: 50 };
+    const margin = { top: 20, right: 50, bottom: 20, left: 50 };
     const width = 1200 - margin.left - margin.right;
     const height = 380 - margin.top - margin.bottom;
 
